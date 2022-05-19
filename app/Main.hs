4e8e9eb43581463503b379da3f1228928d7a4c6c
Marek Kacmar <kacmar.marek@gmail.com>
2022-05-19 13:08:26 +0200

    Added parsing from file into data class Pages

--- app/Main.hs
+++ app/Main.hs
@@ -1,30 +1,50 @@
 {-# START_FILE Main.hs #-}
 {-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
+{-# OPTIONS_GHC -Wno-deferred-out-of-scope-variables #-}
 
 module Main where
 
-import System.IO ()
-import Parser ( parseWebSite )
-import Data.Aeson
+import System.IO (openFile, IOMode (WriteMode), hPutStrLn)
+import Parser
 import Data.Text
 import GHC.Generics
+import Data.Aeson.Types (parseJSON)
+import Data.Aeson
+import Control.Applicative
+import Control.Monad
+
+
+
+
+
 
 data Pages =
   Pages {url :: !Text, html_content :: !Text} deriving (Show,Generic)
 
-data Person = Person { name :: Text, age :: Int } deriving (Generic, Show)
 
 instance FromJSON Pages
 instance ToJSON Pages
 
+
+
+
 main :: IO ()
 main = do
-
-
    --PARSING--
-   let fileName = "data/output.jl" -- After complete parser change filename for "data/collection.jl" instead "data/output.jl"
-   parsedWeb <- Parser.parseWebSite fileName -- parsedWeb - contain parsed web text
-
+   --let fileName = "data/output.json" -- After complete parser change filename for "data/collection.jl" instead "data/output.jl"
+   --parsedWeb <- Parser.parseWebSite fileName -- parsedWeb - contain parsed web text--
+   d <- (eitherDecode <$> getJSON) :: IO (Either String [Pages])
+   case d of
+     Left err -> putStrLn err
+     Right pages -> do
+      -- pages je pole datovych tried [Pages]
+       print . html_content $ Prelude.head pages
+       
+    -- If d is Left, the JSON was malformed.
+    -- In that case, we report the error.
+    -- Otherwise, we perform the operation of
+    -- our choice. In this case, just print it.
+  
    putStrLn $ "Decode: " ++ (show (decode "{ \"url\": \"example.com\", \"html_content\": \"<div>asdf</div>\" }" :: Maybe Pages))
 
    --INDEXER--
--- src/Parser.hs
+++ src/Parser.hs
@@ -1,21 +1,34 @@
 module Parser
-    ( parseWebSite
+    ( loadFile
+    , jsonFile
+    , getJSON
+    , parseWebSite
     ) where
 
+
 import System.IO ( hGetLine, openFile, IOMode(ReadMode) )
+import qualified Data.ByteString.Lazy as B
 
 {-
     TODO:
         - parsing each web page
 -}
 
+loadFile :: FilePath -> IO B.ByteString
+loadFile = B.readFile
+
+jsonFile :: FilePath
+jsonFile = "data/output.json"
+
+getJSON :: IO B.ByteString
+getJSON = B.readFile jsonFile
 
 -- Main function for parsing web data
 parseWebSite :: FilePath -> IO String
 parseWebSite fileName = do
     -- Load file
     fileText <- openFile fileName ReadMode
-    fileName <- hGetLine fileText
+    fileName <- System.IO.hGetLine fileText
     print "Parser - Done!"
 
     return fileName
