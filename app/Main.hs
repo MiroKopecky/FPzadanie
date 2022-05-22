c595709db26895841e65bf2297cbe5872867dd4e
PatrikK <74940369+patrik-kupculak@users.noreply.github.com>
2022-05-22 04:40:38 +0200

    Main - split content, not working on jl yet
    Haskell is pain

--- app/Main.hs
+++ app/Main.hs
@@ -15,6 +15,8 @@ import Control.Monad
 
 import Text.HTML.Scalpel
 import Control.Applicative
+import Data.Text as TS
+import Data.Typeable
 
 import qualified Data.ByteString.Lazy as B
 
@@ -31,9 +33,8 @@ instance FromJSON Page where
       url <- v .: "url"
       html_content <- v .: "html_content"
       return (Page {url = url, html_content = html_content})
-   parseJSON _ = empty
+   parseJSON _ = Control.Applicative.empty
 instance ToJSON Page
-
 
 parsePage :: [B.ByteString] -> [[Text]]
 parsePage [] = []
@@ -49,16 +50,44 @@ parsePage (x:xs) = do
              let parsedText = parseText x pattern
              parsedText : parsePage xs
 
+--https://hackage.haskell.org/package/scalpel-0.6.2/docs/Text-HTML-Scalpel.html
+
+getUrl a = ((url a), scrapeStringLike (html_content a) (texts(tagSelector "div")))
+
+getPageContent a = Prelude.map (\x -> getUrl x) a
+
+parsingText a = parseText a pattern
+
+getParsed a = Prelude.map (parsingText) a
+
 main :: IO ()
 main = do
    --PARSING--
    content <- getJSON
    let splitContent = (B.split 10 content)
+
+   print splitContent
+   --TODO: splitContent need transform from '[B.ByteString]' to '[Page]'
+
+   {-
+   -- TODO: Rework this code for .jl file
+   let scrapedDivs = getPageContent splitContent
+   print scrapedDivs
+   putStrLn ""
+   
+  
+   case (snd (Prelude.head scrapedDivs)) of
+    Nothing -> putStrLn "err"
+    Just x -> do
+      let t = TS.pack $ Prelude.head x
+      let parsedText = getParsed [[t]]
+      print parsedText
+  -}
+
    -- vypise naparsovanu prvu stranku
-   print (head (parsePage splitContent))
+   -- print (head (parsePage splitContent))
 
    --INDEXER--
 
    --PAGE RANK--
-
    putStrLn "Done!"
